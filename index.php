4990125d870ff578f5daaf1fee398a1788b91a93
indra1109 <indrasjk@gmail.com>
2020-06-16 17:20:51 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,12 @@
 <body>
     <h1>Hello, World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate, perferendis. Aperiam veritatis quidem, nihil perferendis repudiandae provident voluptates laudantium incidunt, repellendus sequi totam rem optio, sed labore tempora nostrum error.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+    </ul>
 </body>
 
 </html>
